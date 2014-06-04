7f7ad58e51fbce4db6f5b20a1811e3bf98440cd5
Sangdon Park <sangdonp@cis.upenn.edu>
2014-06-03 21:13:45 -0400

    updates

--- trainJointBoost_mult.m
+++ trainJointBoost_mult.m
@@ -2,24 +2,32 @@ function [ o_mdl ] = trainJointBoost_mult( i_params, i_featFunc, i_featFuncParam
 %LEARNGENTLEBOOST learn gentleboost using axis-parallel linear functions
 %   Detailed explanation goes here
 
-
 nThread = 32;
 nWeakLearner = i_params.nWeakLearner;
 nData = i_params.nData;
 nCls = i_params.nCls;
 rndFeatSelRatio = i_params.rndFeatSelRatio;
+algType = i_params.alg;
+nJB = i_params.nJB;
+if algType ~= int32(2)
+    nJB = int32(1);
+end
+
 
 labels = ones(nData, nCls)*(-1);
 Hs = zeros(nData, nCls);
 ws = ones(nData, nCls);
 
 mdls = struct('a', 0, 'b', 0, 'f', 0, 'theta', 0, 'kc', zeros(1, nCls), 'S', false(1, nCls));
-mdls = repmat(mdls, [nWeakLearner, 1]);
+mdls = repmat(mdls, [nWeakLearner*nJB, 1]);
 
 %% init labels
 parfor (dInd=1:size(labels, 1), nThread)
 % for dInd=1:size(labels, 1)
     if isnan(i_labels(dInd))
+        continue;
+    end
+    if i_labels(dInd) == nCls % bg
         continue;
     end
     dthLabels = labels(dInd, :);
@@ -30,7 +38,7 @@ end
 
 %% consider previous weak classifiers
 for m=1:numel(i_mdl)
-    hs = geths(i_params, i_featFunc, i_featFuncParams, i_mdl(m));
+    hs = geths(i_params, i_featFuncParams, i_mdl(m));
     
     Hs = bsxfun(@plus, Hs, hs);
     ws = bsxfun(@times, ws, exp(-bsxfun(@times, labels, hs))); 
@@ -38,20 +46,65 @@ end
 
 
 %% train weak classifiers
-for m=1:nWeakLearner
-    fprintf('* boosting iter: %d/%d...\n', int32(m), int32(nWeakLearner));
-    
-    if rndFeatSelRatio == 1 %% what happens on the Matlab Coder! cannot beautify my codes
-        [mdls(m), hs] = fitStump(i_params, i_featFunc, i_featFuncParams, labels, ws, 1:i_params.featDim);
-    else
-        rndFeatInd = randperm(i_params.featDim, max(1, round(i_params.featDim*rndFeatSelRatio)));
-        [mdls(m), hs] = fitStump_rnd(i_params, i_featFuncParams, labels, ws, rndFeatInd);
-    end
-    
-    Hs = bsxfun(@plus, Hs, hs);
-    ws = bsxfun(@times, ws, exp(-bsxfun(@times, labels, hs))); 
-    
+switch algType
+    case 0 % no ranomization
+        for m=1:nWeakLearner
+            fprintf('* boosting iter: %d/%d...\n', int32(m), int32(nWeakLearner));
+            
+            % what happens on the Matlab Coder! cannot beautify my codes
+%             [mdls(m), hs] = fitStump_rnd(i_params, i_featFuncParams, labels, ws, 1:i_params.featDim);
+            [mdls(m), hs] = fitStump(i_params, i_featFunc, i_featFuncParams, labels, ws, 1:i_params.featDim);
+            Hs = bsxfun(@plus, Hs, hs);
+            ws = bsxfun(@times, ws, exp(-bsxfun(@times, labels, hs))); 
+        end
+
+    case 1 % randomzation
+        for m=1:nWeakLearner
+            fprintf('* boosting iter: %d/%d...\n', int32(m), int32(nWeakLearner));
+
+            rndFeatInd = randperm(i_params.featDim, max(1, round(i_params.featDim*rndFeatSelRatio)));
+            [mdls(m), hs] = fitStump_rnd(i_params, i_featFuncParams, labels, ws, rndFeatInd, []);
+            Hs = bsxfun(@plus, Hs, hs);
+            ws = bsxfun(@times, ws, exp(-bsxfun(@times, labels, hs))); 
+        end
+        
+    case 2 % randomization+multiple JB
+%         for jbInd=1:nJB
+%             rndFeatInd = randperm(i_params.featDim, max(1, round(i_params.featDim*rndFeatSelRatio)));
+%             % pre-calculate features
+%             redFeatDim = numel(rndFeatInd);
+%             ithfeats_precomp = zeros(nData, redFeatDim, 'single');
+%             parfor (i=1:redFeatDim, nThread)    
+%                 ithfeats_precomp(:, i) = getithFeat_JB( i_featFuncParams, rndFeatInd(i) ); % cannot beautify due to the Matlab Coder. It's more efficient
+%             end
+% 
+%             for m=1:nWeakLearner
+%                 fprintf('* boosting iter: %d/%d...\n', int32(m), int32(nWeakLearner));
+% 
+%                 % fit stumps
+%                 [mdls((jbInd-1)*nWeakLearner + m), hs] = fitStump_rnd(i_params, i_featFuncParams, labels, ws, rndFeatInd, ithfeats_precomp);
+%                 Hs = bsxfun(@plus, Hs, hs);
+%                 ws = bsxfun(@times, ws, exp(-bsxfun(@times, labels, hs))); 
+%             end
+%         end
+        
 end
+
+% for m=1:nWeakLearner
+%     fprintf('* boosting iter: %d/%d...\n', int32(m), int32(nWeakLearner));
+%     
+%     
+%     if rndFeatSelRatio == 1 %% what happens on the Matlab Coder! cannot beautify my codes
+%         [mdls(m), hs] = fitStump(i_params, i_featFunc, i_featFuncParams, labels, ws, 1:i_params.featDim);
+%     else
+%         rndFeatInd = randperm(i_params.featDim, max(1, round(i_params.featDim*rndFeatSelRatio)));
+%         [mdls(m), hs] = fitStump_rnd(i_params, i_featFuncParams, labels, ws, rndFeatInd);
+%     end
+%     
+%     Hs = bsxfun(@plus, Hs, hs);
+%     ws = bsxfun(@times, ws, exp(-bsxfun(@times, labels, hs))); 
+%     
+% end
 
 %% return
 if isempty(i_mdl)
@@ -61,12 +114,13 @@ else
 end
 end
 
-function [o_hs] = geths(i_params, i_featFunc, i_featFuncParams, i_mdl)
+function [o_hs] = geths(i_params, i_featFuncParams, i_mdl)
 nData = i_params.nData;
 nCls = i_params.nCls;
 
 %% reconstruct hs
-delta_pos = i_featFunc(i_featFuncParams, 1:nData, i_mdl.f)>i_mdl.theta;
+delta_pos = getithFeat_JB(i_featFuncParams, i_mdl.f)>i_mdl.theta;
+% delta_pos = i_featFunc(i_featFuncParams, 1:nData, i_mdl.f)>i_mdl.theta;
 o_hs = zeros(nData, nCls);
 o_hs(:, i_mdl.S) = repmat(i_mdl.a*delta_pos + i_mdl.b*(~delta_pos), [1, sum(i_mdl.S)]);
 o_hs(:, ~i_mdl.S) = repmat(i_mdl.kc(~i_mdl.S), [nData, 1]);
@@ -212,7 +266,7 @@ o_hs = besths_S;
 end
 
 
-function [o_mdl, o_hs] = fitStump_rnd(i_params, i_featFuncParams, i_labels, i_ws, i_featInd)
+function [o_mdl, o_hs] = fitStump_rnd(i_params, i_featFuncParams, i_labels, i_ws, i_featInd, i_ithfeats_precomp)
 
 nThread = 32;
 
@@ -225,25 +279,29 @@ nCls = i_params.nCls;
 mdl_init = struct('a', 0, 'b', 0, 'f', 0, 'theta', 0, 'kc', zeros(1, nCls), 'S', false(1, nCls));
 bestCost_S = inf;
 bestMdl_S = mdl_init;
-besths_S = zeros(nData, nCls);
+% besths_S = zeros(nData, nCls);
 
 
 %% precalc features
 redFeatDim = numel(i_featInd);
 ithfeats_precomp = zeros(nData, redFeatDim, 'single');
-parfor (i=1:numel(i_featInd), nThread)    
-    ithfeats_precomp(:, i) = getithFeat_JB( i_featFuncParams, i_featInd(i) ); % cannot beautify due to the Matlab Coder. It's more efficient
+if isempty(i_ithfeats_precomp)
+    parfor (i=1:numel(i_featInd), nThread)    
+        ithfeats_precomp(:, i) = getithFeat_JB( i_featFuncParams, i_featInd(i) ); % cannot beautify due to the Matlab Coder. It's more efficient
+    end
+else
+    ithfeats_precomp = i_ithfeats_precomp;
 end
 
 % greedly finding S(n)
 S = false(1, nCls);
 a_leaf = zeros(1, nCls, numel(i_featInd), numel(featValRange));
 b_leaf = zeros(1, nCls, numel(i_featInd), numel(featValRange));
-for n=1:nCls    
+for n=1:nCls-1 % bg    
     bestCost_ft = inf;
     bestMdl_ft = mdl_init;
-    besths_ft = zeros(nData, nCls);
-    for c=1:nCls
+%     besths_ft = zeros(nData, nCls);
+    for c=1:nCls-1 % bg
         curS = S;
         if curS(c)
             continue;
@@ -267,7 +325,7 @@ for n=1:nCls
         % estimate a, b, f, theta                
         bestCosts_ft = zeros(redFeatDim, 1); % parfor friendly code
         bestMdls_ft = repmat(mdl_init, [redFeatDim, 1]); % parfor friendly code
-        bestths_ft = zeros(redFeatDim, 1); % parfor friendly code
+%         bestths_ft = zeros(redFeatDim, 1); % parfor friendly code
 
         parfor (rfInd=1:redFeatDim, nThread)
 %         warning('dbg');
@@ -277,7 +335,7 @@ for n=1:nCls
 
             bestCost_tmp = inf;
             bestMdl_tmp = mdl_init;
-            bestth_tmp = inf;
+%             bestth_tmp = inf;
             a_leaf_tmp = a_leaf(1, curS, rfInd, :);
             b_leaf_tmp = b_leaf(1, curS, rfInd, :);
             for tInd=1:numel(featValRange)
@@ -320,14 +378,14 @@ for n=1:nCls
                     bestMdl_tmp.kc = ks;
                     bestMdl_tmp.S = curS;
 
-                    bestth_tmp = tInd;
+%                     bestth_tmp = tInd;
                 end
             end
 
             % save
             bestCosts_ft(rfInd) = bestCost_tmp;
             bestMdls_ft(rfInd) = bestMdl_tmp;
-            bestths_ft(rfInd) = bestth_tmp;
+%             bestths_ft(rfInd) = bestth_tmp;
             
             a_leaf(1, curS, rfInd, :) = a_leaf_tmp;
             b_leaf(1, curS, rfInd, :) = b_leaf_tmp;
@@ -337,22 +395,22 @@ for n=1:nCls
         if minVal < bestCost_ft
             bestCost_ft = minVal;
             bestMdl_ft = bestMdls_ft(minInd);
-            bestth = bestths_ft(minInd);
+%             bestth = bestths_ft(minInd);
             
-            minfInd = i_featInd(minInd);
-            delta_pos = getithFeat_JB(i_featFuncParams, minfInd)>featValRange(bestth);
+%             minfInd = i_featInd(minInd);
+%             delta_pos = getithFeat_JB(i_featFuncParams, minfInd)>featValRange(bestth);
 %             delta_pos = ithfeats_precomp(:, minInd) > featValRange(bestth); % Matlab Coder crashes
-            hs = zeros(nData, nCls);
-            hs(:, curS) = repmat(bestMdl_ft.a*(delta_pos) + bestMdl_ft.b*(~(delta_pos)), [1, sum(curS)]);
-            hs(:, ~curS) = repmat(bestMdl_ft.kc(~curS), [nData, 1]);
-            besths_ft = hs;
+%             hs = zeros(nData, nCls);
+%             hs(:, curS) = repmat(bestMdl_ft.a*(ithfeats_precomp(:, minInd) > featValRange(bestth)) + bestMdl_ft.b*(~(ithfeats_precomp(:, minInd) > featValRange(bestth))), [1, sum(curS)]);
+%             hs(:, ~curS) = repmat(bestMdl_ft.kc(~curS), [nData, 1]);
+%             besths_ft = hs;
         end
     end
     
     if bestCost_S > bestCost_ft
         bestCost_S = bestCost_ft;
         bestMdl_S = bestMdl_ft;
-        besths_S = besths_ft;
+%         besths_S = besths_ft;
         
         S = bestMdl_S.S;
     end
@@ -360,7 +418,8 @@ end
 
 %% return
 o_mdl = bestMdl_S;
-o_hs = besths_S;
+% o_hs = besths_S;
+o_hs = geths(i_params, i_featFuncParams, o_mdl);
 
 end
 
